d4777fd362a54a04ba3fa3ff14b2f2f955dbb62b
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2024-10-26 23:06:14 +0800

    fix: zero padding not woking

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import products from "./data.json";
 
 export default function Home() {
   return (
-    <div className="grid xl:grid-cols-content-sidebar gap-400 max-w-9000 mx-auto py-1100 px-500 md:px-0">
+    <div className="grid xl:grid-cols-content-sidebar gap-400 max-w-9000 mx-auto py-1100 px-500 xl:px-0">
       <main role="main">
         <h1 className="sr-only">Frontend Mentor | Product list with cart</h1>
         <h2 className="font-bold text-xl mb-400">Desserts</h2>
