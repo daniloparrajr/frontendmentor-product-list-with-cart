70b07ac5fcfb8437de4a32f9eb8b2d4ad99376ae
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2024-10-30 17:09:01 +0800

    fix: main tag doesn't need main role

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <div className="grid xl:grid-cols-content-sidebar gap-400 max-w-9000 mx-auto py-1100 px-500 xl:px-0">
-      <main role="main">
+      <main>
         <h1 className="sr-only">Frontend Mentor | Product list with cart</h1>
         <h2 className="font-bold text-xl mb-400">Desserts</h2>
         <ProductList
